f348dc88d75e0f49d490338ab4f7c7d805b51b29
Tu Nguyen <tu.nguyen@go.tarleton.edu>
2018-07-02 14:40:40 -0500

    new updates

--- main3.py
+++ main3.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from time import sleep
+import pandas as pd
 
 # REGEX compiled expressions
 tweet_re = re.compile(r'#[a-zA-z]+[0-9]+')
@@ -40,26 +41,29 @@ def get_goals():
 # Partners
 def get_partners():
     partner_index = home_right_raw_lst.index('Partners') + 1
-    next_index = home_right_raw_lst.index('Ocean Basins')                     if len(r.history) and 'the ocean conference' in soup.title.text.lower()                     else home_right_raw_lst.index('Countries')
+    next_index = home_right_raw_lst.index('Ocean Basins') \
+                    if len(r.history) and 'the ocean conference' in soup.title.text.lower() \
+                    else home_right_raw_lst.index('Countries')
             
     partners = [p.strip() for p in home_right_raw_lst[partner_index:next_index]]
     partners = list(filter(None, partners))
     
-    return ' || '.join(partners)
+    return '; '.join(partners)
 
 # Description
 def get_description():
     des_raw = soup.find(id='intro').find('div', attrs={'class':'wrap'})
 
     temp = str(des_raw)
-
+    
+    temp = re.sub(htmltag_re, '', temp)
+    temp = re.sub(newline_re, '', temp)
+    
     if '<div id="subHeadline">' in temp:
-        temp = re.sub(des_separator_re, ' : ', temp)
-        temp = re.sub(htmltag_re, '', temp)
-        temp = re.sub(newline_re, '\n', temp)
+        temp = re.sub(des_separator_re, ' : ', temp)    
         return temp.strip()
     else: 
-        return des_raw.get_text().strip()
+        return temp.strip()
     
 def get_resources():
     resources_raw = soup.find(id='resources')
@@ -72,7 +76,7 @@ def get_resources():
             temp = re.sub(r'\n+', ' : ', temp.strip())
 #             temp = re.sub(r'[\x91\x92]', '\'', temp)
             resources_lst.append(temp.strip())
-    return ' || '.join(resources_lst)
+    return '; '.join(resources_lst)
 
 
 def get_timeframe():
@@ -106,11 +110,15 @@ ids = open('good_ids.txt').read().split()
 # open file
 data_file = open('data.csv', 'w')
 
-data_file.write('Title\tGoals\tPartners\tDescription\tResources\tTime_frame\tCountries\tHashtag\n')
+data_file.write('Project_idx\tTitle\tGoals\tPartners\tDescription\tResources\tTime_frame\tCountries\tHashtag\n')
+
+df_raw = []
 
 # start collecting data
-
 for ide in ids:
+
+    project_idx = '0'*(5 - len(ide)) + ide
+
     url = base_url + ide
     success_connect = False
     
@@ -134,9 +142,21 @@ for ide in ids:
     home_right_raw_lst = home_right.getText().split('\n')
     home_right_raw_lst = list(filter(None, home_right_raw_lst))
     
-    row = [repr(f()) for f in functions]
-    row = '\t'.join(row)
-    row = row + '\n'
-    data_file.write(row)
+    row = [f() for f in functions]
+    
+    row.insert(0, repr(project_idx))
+    
+    df_raw.append(row)
+
+df = pd.DataFrame(df_raw, columns=['Project_idx', 'Title','Goals', 'Partners', \
+                                  'Description', 'Resources', 'Time_frame' ,'Countries', 'Hashtag'] )
+
+writer = pd.ExcelWriter('output.xlsx')
+df.to_excel(writer, index=False)
+writer.save()
+
+
+
+
     
 data_file.close()
